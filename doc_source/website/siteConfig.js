8daf567e38d4a69695c934bccd93943519757a4b
natezimmer <nate.m.zimmerman@gmail.com>
2019-08-11 17:48:54 -0500

    plz work

--- doc_source/website/siteConfig.js
+++ doc_source/website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'Making docs',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://natezimmer.github.io/', // Your website URL
+  baseUrl: '/docusaurusTest/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
